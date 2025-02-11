cd7a099f2383d5709ca44b47e369a91c1bff94f5
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2025-02-11 23:52:06 +0900

    Biomeの初期設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
+import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
 // @ts-check
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
