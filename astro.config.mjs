721a0bffc1c30f3c87f13e38ea659c1d588e9871
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2025-02-11 23:29:37 +0900

    Prettierの初期設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,26 +7,26 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      starlight({
-          title: 'My Docs',
-          social: {
-              github: 'https://github.com/withastro/starlight',
-          },
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
           ],
-      }),
-	],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
