a7266124ffc8183907dfaf7de964f3727101eb54
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2025-05-01 11:36:13 +0900

    Showコンポーネント内のresult参照方法を修正

--- src/components/appearance-statistics/Output.tsx
+++ src/components/appearance-statistics/Output.tsx
@@ -4,38 +4,42 @@ import { store } from './store';
 export const Output = () => {
   return (
     <Show when={store.result}>
-      <div>
-        <span>{store.result?.eventCount}件のイベントを検索しました。</span>
-        <a
-          href={store.result?.searchUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          検索結果
-        </a>
-      </div>
-      <div class="overflow-x-auto">
-        <table class="d-table d-table-zebra">
-          <thead>
-            <tr>
-              <th />
-              <th class="text-center">出演者名</th>
-              <th class="text-right">出演数</th>
-            </tr>
-          </thead>
-          <tbody>
-            <For each={store.result?.statistics}>
-              {([actorName, count], index) => (
+      {(result) => (
+        <>
+          <div>
+            <span>{result().eventCount}件のイベントを検索しました。</span>
+            <a
+              href={result().searchUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              検索結果
+            </a>
+          </div>
+          <div class="overflow-x-auto">
+            <table class="d-table d-table-zebra">
+              <thead>
                 <tr>
-                  <th class="text-right">{index() + 1}</th>
-                  <td class="text-center">{actorName}</td>
-                  <td class="text-right">{count}</td>
+                  <th />
+                  <th class="text-center">出演者名</th>
+                  <th class="text-right">出演数</th>
                 </tr>
-              )}
-            </For>
-          </tbody>
-        </table>
-      </div>
+              </thead>
+              <tbody>
+                <For each={result().statistics}>
+                  {([actorName, count], index) => (
+                    <tr>
+                      <th class="text-right">{index() + 1}</th>
+                      <td class="text-center">{actorName}</td>
+                      <td class="text-right">{count}</td>
+                    </tr>
+                  )}
+                </For>
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
     </Show>
   );
 };
