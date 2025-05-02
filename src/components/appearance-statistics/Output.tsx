1e4cdaf9298278fc0c2515af2dbc8cd05fda96f3
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2025-05-02 23:24:38 +0900

    不要な波括弧を除去

--- src/components/appearance-statistics/Output.tsx
+++ src/components/appearance-statistics/Output.tsx
@@ -1,45 +1,43 @@
 import { For, Show } from 'solid-js';
 import { response } from './store';
 
-export const Output = () => {
-  return (
-    <Show when={response()}>
-      {(response) => (
-        <>
-          <div>
-            <span>{response().eventCount}件のイベントを検索しました。</span>
-            <a
-              href={response().searchUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              検索結果
-            </a>
-          </div>
-          <div class="overflow-x-auto">
-            <table class="d-table d-table-zebra">
-              <thead>
-                <tr>
-                  <th />
-                  <th class="text-center">出演者名</th>
-                  <th class="text-right">出演数</th>
-                </tr>
-              </thead>
-              <tbody>
-                <For each={response().statistics}>
-                  {([actorName, count], index) => (
-                    <tr>
-                      <th class="text-right">{index() + 1}</th>
-                      <td class="text-center">{actorName}</td>
-                      <td class="text-right">{count}</td>
-                    </tr>
-                  )}
-                </For>
-              </tbody>
-            </table>
-          </div>
-        </>
-      )}
-    </Show>
-  );
-};
+export const Output = () => (
+  <Show when={response()}>
+    {(response) => (
+      <>
+        <div>
+          <span>{response().eventCount}件のイベントを検索しました。</span>
+          <a
+            href={response().searchUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            検索結果
+          </a>
+        </div>
+        <div class="overflow-x-auto">
+          <table class="d-table d-table-zebra">
+            <thead>
+              <tr>
+                <th />
+                <th class="text-center">出演者名</th>
+                <th class="text-right">出演数</th>
+              </tr>
+            </thead>
+            <tbody>
+              <For each={response().statistics}>
+                {([actorName, count], index) => (
+                  <tr>
+                    <th class="text-right">{index() + 1}</th>
+                    <td class="text-center">{actorName}</td>
+                    <td class="text-right">{count}</td>
+                  </tr>
+                )}
+              </For>
+            </tbody>
+          </table>
+        </div>
+      </>
+    )}
+  </Show>
+);
--- src/components/coacting-events/Output.tsx
+++ src/components/coacting-events/Output.tsx
@@ -1,33 +1,31 @@
 import { For, Show } from 'solid-js';
 import { response } from './store';
 
-export const Output = () => {
-  return (
-    <Show when={response()}>
-      {(response) => (
-        <>
-          <p>{response().events.length}件のイベントが見つかりました。</p>
-          <ul class="d-list">
-            <For each={response().events}>
-              {(event) => (
-                <li class="d-list-row">
-                  <div>
-                    <a
-                      href={`https://www.eventernote.com${event.href}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {event.name}
-                    </a>
-                    <div class="text-xs">{event.date}</div>
-                    <div class="text-xs">{event.place}</div>
-                  </div>
-                </li>
-              )}
-            </For>
-          </ul>
-        </>
-      )}
-    </Show>
-  );
-};
+export const Output = () => (
+  <Show when={response()}>
+    {(response) => (
+      <>
+        <p>{response().events.length}件のイベントが見つかりました。</p>
+        <ul class="d-list">
+          <For each={response().events}>
+            {(event) => (
+              <li class="d-list-row">
+                <div>
+                  <a
+                    href={`https://www.eventernote.com${event.href}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {event.name}
+                  </a>
+                  <div class="text-xs">{event.date}</div>
+                  <div class="text-xs">{event.place}</div>
+                </div>
+              </li>
+            )}
+          </For>
+        </ul>
+      </>
+    )}
+  </Show>
+);
