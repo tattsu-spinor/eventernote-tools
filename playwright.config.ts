daa31a2db25a7d779307a194ce9d347f55452d32
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2025-10-22 01:38:45 +0900

    E2Eテストを追加 (#77)
    * Playwrightの設定
    
    * E2Eテストを追加
    
    * GitHub ActionsにPlaywrightテスト用ワークフローを追加
    
    * Playwrightワークフローにpermissionsセクションを追加

---
+++ playwright.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@playwright/test';
+
+// https://playwright.dev/docs/test-configuration
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  retries: 1,
+  reporter: 'html',
+  use: {
+    baseURL: process.env.BASE_URL || 'http://localhost:4321',
+    extraHTTPHeaders: {
+      'x-vercel-protection-bypass': process.env.VERCEL_PROTECTION_BYPASS || '',
+    },
+    trace: 'on-first-retry',
+  },
+});
