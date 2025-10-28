cce8aaa5265386ae3eacc2a9a2df55a1ae00c24e
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2025-10-28 23:12:13 +0900

    入力フォームのマークアップを修正

--- src/components/attended-events/Input.tsx
+++ src/components/attended-events/Input.tsx
@@ -7,68 +7,66 @@ export const Input = () => {
   const canNotSearch = () => !inputStore.userId;
 
   return (
-    <>
-      <fieldset class="grid gap-4 max-w-xs sm:max-w-sm  md:max-w-md">
-        <label class="d-floating-label">
-          <span>ユーザーID</span>
-          <input
-            name="userId"
-            type="text"
-            placeholder="ユーザーID"
-            value={inputStore.userId}
-            onInput={(e) => {
-              setInputStore('userId', e.target.value);
-            }}
-            class="d-input"
-          />
-        </label>
+    <form class="grid gap-4 p-2 max-w-xs sm:max-w-sm  md:max-w-md">
+      <label class="d-floating-label">
+        <span>ユーザーID</span>
+        <input
+          name="userId"
+          type="text"
+          placeholder="ユーザーID"
+          value={inputStore.userId}
+          onInput={(e) => {
+            setInputStore('userId', e.target.value);
+          }}
+          class="d-input"
+        />
+      </label>
 
-        <label class="d-floating-label">
-          <span>出演者名</span>
-          <input
-            name="actorName"
-            type="text"
-            placeholder="出演者名（オプション）"
-            value={inputStore.actorName}
-            onInput={(e) => {
-              setInputStore('actorName', e.target.value);
-            }}
-            class="d-input"
-          />
-        </label>
+      <label class="d-floating-label">
+        <span>出演者名</span>
+        <input
+          name="actorName"
+          type="text"
+          placeholder="出演者名（オプション）"
+          value={inputStore.actorName}
+          onInput={(e) => {
+            setInputStore('actorName', e.target.value);
+          }}
+          class="d-input"
+        />
+      </label>
 
-        <label class="d-floating-label">
-          <span>会場名</span>
-          <input
-            name="placeName"
-            type="text"
-            placeholder="会場名（オプション）"
-            value={inputStore.placeName}
-            onInput={(e) => {
-              setInputStore('placeName', e.target.value);
-            }}
-            class="d-input"
-          />
-        </label>
+      <label class="d-floating-label">
+        <span>会場名</span>
+        <input
+          name="placeName"
+          type="text"
+          placeholder="会場名（オプション）"
+          value={inputStore.placeName}
+          onInput={(e) => {
+            setInputStore('placeName', e.target.value);
+          }}
+          class="d-input"
+        />
+      </label>
 
-        <div>
-          <button
-            type="button"
-            onClick={search}
-            disabled={outputStore.loading || canNotSearch()}
-            class="d-btn d-btn-primary"
-          >
-            検索
-            <Show when={outputStore.loading}>
-              <span class="d-loading d-loading-spinner" />
-            </Show>
-          </button>
-        </div>
-      </fieldset>
+      <div>
+        <button
+          type="button"
+          onClick={search}
+          disabled={outputStore.loading || canNotSearch()}
+          class="d-btn d-btn-primary"
+        >
+          検索
+          <Show when={outputStore.loading}>
+            <span class="d-loading d-loading-spinner" />
+          </Show>
+        </button>
+      </div>
 
       <Show when={outputStore.error} keyed>
         {(error) => (
-          <div role="alert" class="d-alert d-alert-error my-3">
+          <div role="alert" class="d-alert d-alert-error">
             <span>
               {error.code !== 'INTERNAL_SERVER_ERROR' && error.message
                 ? error.message
@@ -77,6 +75,6 @@ export const Input = () => {
           </div>
         )}
       </Show>
-    </>
+    </form>
   );
 };
