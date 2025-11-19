f3b67364bb7176e260f08ebe84c6f7b7014010a7
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2025-11-20 00:51:52 +0900

    各InputコンポーネントのUI改善

--- src/components/appearance-statistics/Input.tsx
+++ src/components/appearance-statistics/Input.tsx
@@ -10,134 +10,136 @@ export const Input = () => {
       .every((v) => !v);
 
   return (
-    <form class="max-w-xs sm:max-w-sm md:max-w-md">
-      <fieldset class="d-fieldset">
-        <legend class="d-fieldset-label">キーワード</legend>
-        <input
-          type="text"
-          name="keyword"
-          aria-label="キーワード"
-          value={inputStore.keyword}
-          onInput={(e) => {
-            setInputStore('keyword', e.target.value);
-          }}
-          placeholder="声優、アイドル、アーティスト名等"
-          class="d-input w-full"
-        />
-      </fieldset>
+    <form class="d-card p-2 gap-4">
+      <div class="max-w-lg">
+        <fieldset class="d-fieldset">
+          <legend class="d-fieldset-label">キーワード</legend>
+          <input
+            type="text"
+            name="keyword"
+            aria-label="キーワード"
+            value={inputStore.keyword}
+            onInput={(e) => {
+              setInputStore('keyword', e.target.value);
+            }}
+            placeholder="声優、アイドル、アーティスト名等"
+            class="d-input w-full"
+          />
+        </fieldset>
 
-      <fieldset class="d-fieldset">
-        <legend class="d-fieldset-label">開催日</legend>
-        <div class="d-join w-full">
-          <select
-            name="year"
-            aria-label="年"
-            value={inputStore.year}
-            onInput={(e) => {
-              setInputStore('year', e.target.value);
-            }}
-            class="d-join-item d-select"
-          >
-            <option selected value="">
-              {' - '}年
-            </option>
-            <For each={YEARS}>{(n) => <option value={n}>{n}年</option>}</For>
-          </select>
-          <select
-            name="month"
-            aria-label="月"
-            value={inputStore.month}
-            onInput={(e) => {
-              setInputStore('month', e.target.value);
-            }}
-            class="d-join-item d-select"
-          >
-            <option selected value="">
-              {' - '}月
-            </option>
-            <For each={MONTHS}>{(n) => <option value={n}>{n}月</option>}</For>
-          </select>
-          <select
-            name="day"
-            aria-label="日"
-            value={inputStore.day}
-            onInput={(e) => {
-              setInputStore('day', e.target.value);
-            }}
-            class="d-join-item d-select"
-          >
-            <option selected value="">
-              {' - '}日
-            </option>
-            <For each={DAYS}>{(n) => <option value={n}>{n}日</option>}</For>
-          </select>
-        </div>
-      </fieldset>
-
-      <fieldset class="d-fieldset">
-        <legend class="d-fieldset-label">開催地</legend>
-        <div class="d-join">
-          <label class="d-join-item d-swap d-input w-36">
-            <input
-              type="checkbox"
-              name="isPrefectureMode"
-              aria-label="都道府県モード"
-              checked={inputStore.isPrefectureMode}
+        <fieldset class="d-fieldset">
+          <legend class="d-fieldset-label">開催日</legend>
+          <div class="d-join">
+            <select
+              name="year"
+              aria-label="年"
+              value={inputStore.year}
               onInput={(e) => {
-                const isPrefectureMode = e.target.checked;
-                setInputStore('isPrefectureMode', isPrefectureMode);
-                isPrefectureMode
-                  ? setInputStore('areaId', '')
-                  : setInputStore('prefectureId', '');
+                setInputStore('year', e.target.value);
               }}
-            />
-            <span class="d-swap-on">都道府県:</span>
-            <span class="d-swap-off">地域:</span>
-          </label>
-          <Switch>
-            <Match when={inputStore.isPrefectureMode}>
-              <select
-                name="prefectureId"
-                aria-label="都道府県"
-                value={inputStore.prefectureId}
-                onInput={(e) => {
-                  setInputStore('prefectureId', e.target.value);
-                }}
-                class="d-join-item d-select w-full"
-              >
-                <option selected value="">
-                  -
-                </option>
-                <For each={PREFECTURES}>
-                  {(prefecture) => (
-                    <option value={prefecture.id}>{prefecture.name}</option>
-                  )}
-                </For>
-              </select>
-            </Match>
-            <Match when={true}>
-              <select
-                name="areaId"
-                aria-label="地域"
-                value={inputStore.areaId}
-                onInput={(e) => {
-                  setInputStore('areaId', e.target.value);
-                }}
-                class="d-join-item d-select w-full"
-              >
-                <option selected value="">
-                  -
-                </option>
-                <For each={AREAS}>
-                  {(area) => <option value={area.id}>{area.name}</option>}
-                </For>
-              </select>
-            </Match>
-          </Switch>
-        </div>
-      </fieldset>
+              class="d-join-item d-select"
+            >
+              <option selected value="">
+                {' - '}年
+              </option>
+              <For each={YEARS}>{(n) => <option value={n}>{n}年</option>}</For>
+            </select>
+            <select
+              name="month"
+              aria-label="月"
+              value={inputStore.month}
+              onInput={(e) => {
+                setInputStore('month', e.target.value);
+              }}
+              class="d-join-item d-select"
+            >
+              <option selected value="">
+                {' - '}月
+              </option>
+              <For each={MONTHS}>{(n) => <option value={n}>{n}月</option>}</For>
+            </select>
+            <select
+              name="day"
+              aria-label="日"
+              value={inputStore.day}
+              onInput={(e) => {
+                setInputStore('day', e.target.value);
+              }}
+              class="d-join-item d-select"
+            >
+              <option selected value="">
+                {' - '}日
+              </option>
+              <For each={DAYS}>{(n) => <option value={n}>{n}日</option>}</For>
+            </select>
+          </div>
+        </fieldset>
 
-      <div class="p-2">
+        <fieldset class="d-fieldset">
+          <legend class="d-fieldset-label">開催地</legend>
+          <div class="d-join">
+            <label class="d-join-item d-swap d-input w-36">
+              <input
+                type="checkbox"
+                name="isPrefectureMode"
+                aria-label="都道府県モード"
+                checked={inputStore.isPrefectureMode}
+                onInput={(e) => {
+                  const isPrefectureMode = e.target.checked;
+                  setInputStore('isPrefectureMode', isPrefectureMode);
+                  isPrefectureMode
+                    ? setInputStore('areaId', '')
+                    : setInputStore('prefectureId', '');
+                }}
+              />
+              <span class="d-swap-on">都道府県:</span>
+              <span class="d-swap-off">地域:</span>
+            </label>
+            <Switch>
+              <Match when={inputStore.isPrefectureMode}>
+                <select
+                  name="prefectureId"
+                  aria-label="都道府県"
+                  value={inputStore.prefectureId}
+                  onInput={(e) => {
+                    setInputStore('prefectureId', e.target.value);
+                  }}
+                  class="d-join-item d-select w-full"
+                >
+                  <option selected value="">
+                    -
+                  </option>
+                  <For each={PREFECTURES}>
+                    {(prefecture) => (
+                      <option value={prefecture.id}>{prefecture.name}</option>
+                    )}
+                  </For>
+                </select>
+              </Match>
+              <Match when={true}>
+                <select
+                  name="areaId"
+                  aria-label="地域"
+                  value={inputStore.areaId}
+                  onInput={(e) => {
+                    setInputStore('areaId', e.target.value);
+                  }}
+                  class="d-join-item d-select"
+                >
+                  <option selected value="">
+                    -
+                  </option>
+                  <For each={AREAS}>
+                    {(area) => <option value={area.id}>{area.name}</option>}
+                  </For>
+                </select>
+              </Match>
+            </Switch>
+          </div>
+        </fieldset>
+      </div>
+
+      <div class="d-card-actions">
         <button
           type="button"
           onClick={search}
